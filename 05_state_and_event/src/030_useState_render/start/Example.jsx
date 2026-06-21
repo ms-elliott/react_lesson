bd60c603cb047546be9192c4b1f2fc05bda2c633
ms-elliott <ms.elliott55@gmail.com>
2026-06-21 22:26:01 +0900

    Added lessons ~ 05_055_practice_state

--- 05_state_and_event/src/030_useState_render/start/Example.jsx
+++ 05_state_and_event/src/030_useState_render/start/Example.jsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 
 const Example = () => {
   let displayVal;
-  let [ val, setVal ] = useState();
+  // 入力で値が変更された場合、Example()を再実行しないと反映されない。
+  // しかしExample()を再実行すると変更した値も行３のところで初期化される。
+  // その部分を補う機能 = state
+  let [ val, setVal ] = useState();  // useStateで初期化
   console.log('再レンダリングされました');
   return (
     <>
@@ -10,7 +13,7 @@ const Example = () => {
         type="text"
         onChange={(e) => {
           console.log(e.target.value);
-          setVal(e.target.value);
+          setVal(e.target.value);   // 更新した値をセットして保持 (同時にExample()を再実行する)
           // displayVal = e.target.value;
         }}
       />
--- 05_state_and_event/src/050_prev_state/start/Example.jsx
+++ 05_state_and_event/src/050_prev_state/start/Example.jsx
@@ -1,7 +1,25 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+    const [count, setCount] = useState(0)
+    const countUp = () => {
+        setCount(count + 1)
+        // setCount(count + 1) 同じ処理を２回書いても、count=0のままなので、意図した値にはならない。
+        // 反映後の値に対して、何か処理を実行したい場合はprevstateを使用する
+        setCount(prevstate => {
+            return prevstate + 1
+        })
+    }
+    const countDown = () => {
+        setCount(count - 1)
+    }
+  return (
+    <>
+        <p>現在のカウント数: {count}</p>
+        <button onClick={countUp}>+</button>
+        <button onClick={countDown}>-</button>
+    </>
+  )
 };
 
 export default Example;
