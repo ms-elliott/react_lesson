199b26e9422a8dcc5716e1cecb02598b37da54ad
ms-elliott <ms.elliott55@gmail.com>
2026-06-25 22:03:34 +0900

    Added lessons ~ 06_060

--- 06_control_and_form/src/060_conditional_render/start/Example.jsx
+++ 06_control_and_form/src/060_conditional_render/start/Example.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+// if文、＆＆、？？（null合体演算子)、３項演算子
 const Example = () => {
   const animals = ["Dog", "Cat", "Rat"];
 
@@ -19,9 +20,28 @@ const Example = () => {
             console.log(animal.indexOf(filterVal));
             return isMatch;
           })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
+          .map((animal) => {
+        // ??
+        // A ?? B    AがnullorUNDEFINEDの場合は、Bの値を返す
+
+        // &&
+        return <li key={animal}>{
+            animal
+        }{animal === "Dog" && "★"}</li>
+
+        // ３項演算子
+        //    return <li key={animal}>{
+        //     animal + (animal === "Dog"
+        //       ? "★"
+        //         : "")}</li>
+
+        // if文
+        //   if(animal === "Dog") {
+        //     return <li key={animal}>{animal}★</li>
+        //   } else {
+        //     return <li key={animal}>{animal}</li>
+        //   }
+        })}
       </ul>
     </>
   );
