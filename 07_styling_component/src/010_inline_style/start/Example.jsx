59807dc1b199d54892d404240a8c9055a5228e36
ms-elliott <ms.elliott55@gmail.com>
2026-06-29 21:33:30 +0900

    Added lessons ~ 07_styleing_component 030_css_module

--- 07_styling_component/src/010_inline_style/start/Example.jsx
+++ 07_styling_component/src/010_inline_style/start/Example.jsx
@@ -4,11 +4,22 @@ const Example = () => {
     const [isSelected, setIsSelected] = useState(false);
 
     const clickHandler = () => setIsSelected(prev => !prev);
+    const style = {
+        width: 120, // 単位を省略すると、自動的にpxで設定される
+        height: 60,
+        display: "block",       // 文字列の場合はダブルクォーテーションで囲む
+        fontWeight: "bold",     // CSSではハイフンでつなぐプロパティ(ケバブケース/font-weight)だが、JSではキャネルケース(fontWeight)で記述する
+        borderRadius: 9999,     // ハイフンでプロパティを記述したい場合は、ダブルクォーテーションで囲む
+        cursor: "pointer",
+        border: "none",
+        margin: "auto",
+        background: isSelected ? "pink" : ""
+    }
 
     return (
         <>
-            <button onClick={clickHandler}>ボタン</button>
-            <div>{isSelected && "クリックされました。"}</div>
+            <button onClick={clickHandler} style={style}>ボタン</button>
+            <div style={{ textAlign : "center" }}>{isSelected && "クリックされました。"}</div>
         </>
     )
 };
