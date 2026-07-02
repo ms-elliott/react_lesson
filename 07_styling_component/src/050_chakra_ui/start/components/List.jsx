8abfcf2d044ecf44f684da71059737b3e9a9bff3
ms-elliott <ms.elliott55@gmail.com>
2026-07-02 20:05:34 +0900

    Completed lesson 07

--- 07_styling_component/src/050_chakra_ui/start/components/List.jsx
+++ 07_styling_component/src/050_chakra_ui/start/components/List.jsx
@@ -1,18 +1,32 @@
+import { Text, HStack, StackDivider, VStack, IconButton } from "@chakra-ui/react";
+import { VscCheck } from "react-icons/vsc"
+
 const List = ({todos, deleteTodo}) => {
     const complete = (id) => {
         deleteTodo(id)
     }
     return (
-        <div>
+        <VStack
+            divider={<StackDivider />}
+            // color={{ sm: 'red.600', md: 'blue.600', lg: 'green.500', xl: 'red.600'}}    // mediaクエリの代わりに使用可能
+            borderColor="black.100"
+            borderWidth="1px"
+            borderRadius="3px"
+            p={5}
+            alignItems="start"
+
+        >
             {todos.map(todo => {
                 return (
-                    <div key={todo.id}>
-                        <button onClick={() => complete(todo.id)}>完了</button>
-                        <span>{todo.content}</span>
-                    </div>
+                    <HStack key={todo.id} spacing="5">
+                        <IconButton onClick={() => complete(todo.id)} icon={<VscCheck />} isRound="true"
+                            bgColor="cyan.100" opacity="0.5"
+                        >完了</IconButton>
+                        <Text>{todo.content}</Text>
+                    </HStack>
                 )
             })}
-        </div>
+        </VStack>
     );
 }
 
