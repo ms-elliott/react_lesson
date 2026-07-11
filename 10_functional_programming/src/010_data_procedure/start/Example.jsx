da2723099aa831604d24109f69547bee16a602df
ms-elliott <ms.elliott55@gmail.com>
2026-07-11 21:51:16 +0900

    Added lessons ~ 10_functional_programming_010_data_procedure

--- 10_functional_programming/src/010_data_procedure/start/Example.jsx
+++ 10_functional_programming/src/010_data_procedure/start/Example.jsx
@@ -1,8 +1,30 @@
 const Example = () => {
+    const nums = [1,2,3];
+    const sum = (arry) => {
+            const nums = arry;
+            let sumValue = 0;
+            // for(let i = 0; i < nums.length; i++) {
+            //     sumValue += nums[i];
+            // }
+            nums.forEach(num => sumValue += num);
+            return sumValue;
+    }
+
+    const numObj = {
+        nums: [1,2,3],
+        sum() {
+            const nums = this.nums;
+            let sumValue = 0;
+            for(let i = 0; i < nums.length; i++) {
+                sumValue += nums[i];
+            }
+            return sumValue;
+        }
+    }
   return (
     <>
-      <h3>状態管理と処理を分離</h3>
-      <p>状態（データ）と処理（やりたいこと）は切り離す</p>
+      <div>オブジェクト指向型:{numObj.sum()}</div>
+      <div>関数型:{sum(nums)}</div>
     </>
   );
 };
