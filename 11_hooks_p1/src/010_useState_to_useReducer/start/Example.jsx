977cf36e3a765374478b1c9dc418ec2aafe36619
ms-elliott <ms.elliott55@gmail.com>
2026-07-14 21:57:37 +0900

    Added lessons ~ 11_025_practice_useReducer

--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
@@ -1,7 +1,38 @@
+import { useReducer, useState } from "react";
+
 const Example = () => {
-  
+  const [state, setState] = useState(0);
+  const [rstate, dispatch] = useReducer((prev, action) => {
+    if(action === '+') {
+        return ++prev;
+    } else if(action == '-') {
+        return --prev;
+    }
+  }, 0);
+
+  const countUp = () => {
+    setState(prev => ++prev);
+  }
+
+  const rCountUp = () => {
+    dispatch('+');
+    // dispath()を実行すると、useReducerの第一引数で設定した関数が実行される
+  }
+
+  const rCountDown = () => {
+    dispatch('-');
+  }
   return (
     <>
+        <div>
+        <h3>{state}</h3>
+        <button onClick={countUp}>+</button>
+        </div>
+        <div>
+        <h3>{rstate}</h3>
+        <button onClick={rCountUp}>+</button>
+        <button onClick={rCountDown}>-</button>
+        </div>
     </>
   );
 };
