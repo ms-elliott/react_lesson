4aedf46cde8ab7ec1c67d8b5fbae813827cba560
ms-elliott <ms.elliott55@gmail.com>
2026-07-16 21:13:34 +0900

    Added lessons ~ 11_065_useContext_with_reducer

--- 11_hooks_p1/src/060_context_file_render/start/context/ThemeContext.jsx
+++ 11_hooks_p1/src/060_context_file_render/start/context/ThemeContext.jsx
@@ -1,16 +1,21 @@
 import { useState, useContext, createContext } from "react";
 
+// パフォーマンスが気になってきたら、更新用関数とステートを保持するコンテキストを別で定義する
 export const ThemeContext = createContext();
+export const ThemeUpdateContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
 
   const [theme, setTheme] = useState("light");
-  
+
   return (
-    <ThemeContext.Provider value={[theme, setTheme]}>
-      {children}
+    <ThemeContext.Provider value={theme}>
+        <ThemeUpdateContext.Provider value={setTheme}>
+            {children}
+        </ThemeUpdateContext.Provider>
     </ThemeContext.Provider>
   );
 };
 
 export const useTheme = () => useContext(ThemeContext);
+export const useUpdateTheme = () => useContext(ThemeUpdateContext);
