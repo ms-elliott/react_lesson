3d7436f2ca59621b4f954a438f3c177a824713c6
ms-elliott <ms-elliott55@gmail.com>
2026-08-12 23:41:19 +0900

    Completed 13_redux

---
+++ 13_redux/start/src/070_middleware/store/middleware/logger.js
@@ -0,0 +1,13 @@
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      // 全てのdispatchで行われる処理
+      // storeはaction前の状態
+      // （store.getState()でステートを取得）
+      next(action);
+      // storeはaction後の状態
+    };
+  };
+};
+
+export default logger;
